e733553afd47c61f93946da124f514b076c18f4c
Andrew Brunker <andrew@brunker.net.au>
2024-11-24 00:33:54 +1100

    feat: solution is compiling
    Signed-off-by: Andrew Brunker <andrew@brunker.net.au>

--- consumers/Program.cs
+++ consumers/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace RabbitmqExample.Consumers
+﻿using RabbitmqExample.Common;
+
+namespace RabbitmqExample.Consumers
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Hello World!");
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Consumer : ConsumerBase
+    {
+        public Consumer()
+            : base()
+        {
+            this.QueueNames = new List<string> { "test" };
+        }
+    }
 }
