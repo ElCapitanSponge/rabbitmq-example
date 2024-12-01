6317d0f23738aa01701892d62ca928687f865db4
Andrew Brunker <andrew@brunker.net.au>
2024-12-02 07:07:34 +1100

    refactor: move from .net9 to .net8
    Signed-off-by: Andrew Brunker <andrew@brunker.net.au>

--- publishers/Program.cs
+++ publishers/Program.cs
@@ -2,7 +2,6 @@ using AspNetCore.Swagger.Themes;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -14,19 +13,8 @@ app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
-    app.UseSwaggerUI(ModernStyle.Dark, options =>
-    {
-        options.SwaggerEndpoint("/openapi/v1.json", "RabbitMQ Example Publishers");
-    });
-    /* app.MapScalarApiReference(options => */
-    /* { */
-    /* 	options */
-    /* 		.WithTitle("RabbitMQ Example Publishers") */
-    /* 		.WithTheme(ScalarTheme.Kepler) */
-    /* 		.WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient); */
-    /* }); */
+    app.UseSwaggerUI(ModernStyle.Dark);
 }
 
 app.UseHttpsRedirection();
